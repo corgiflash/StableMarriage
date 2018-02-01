3284a0f3b87dcbe1cc65252be21c7e29e4382346
Dawei <zdwjon@gmail.com>
2018-01-31 20:38:07 -0800

    init project
    init project

--- src/DaweiZhang.java
+++ src/DaweiZhang.java
@@ -25,71 +25,75 @@ class Woman{
 }
 
 public class DaweiZhang {
-   ArrayList<Man> men = new ArrayList<>();
-   ArrayList<Woman> women = new ArrayList<>();
+    ArrayList<Man> men = new ArrayList<>();
+    ArrayList<Woman> women = new ArrayList<>();
 
-   public DaweiZhang(String fileName) {
-      try {
-         readfile(fileName);
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
-   }
+    public DaweiZhang(String fileName) {
+        try {
+            readfile(fileName);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-   public static void main(String args[]) {
-      DaweiZhang dz = new DaweiZhang("input1.txt");//change when you using JGRASP input1.txt
-                                                  //Using src/input.txt for here.
-   
-     //dz.stableM();
-   }
+    public static void main(String args[]) {
+        DaweiZhang dz = new DaweiZhang("input1.txt");//change when you using JGRASP input1.txt
+        //stableM();
+        //Using src/input.txt for here.
+    }
 
-   public void readfile(String fileName) throws Exception {
-      Scanner inFile = new Scanner(new File(fileName));
-      int count = 0;
-      int size = inFile.nextInt();
-      men = new ArrayList<>();
-      women = new ArrayList<>();
-      for (int i = 0; i < size * 2; i++) {
-         // initial the men and women in arrays
-         if (i < size)
-            men.add(new Man(size));
-         else
-            women.add(new Woman(size));
-      }
-      while (inFile.hasNext()) {
-         // initial the rank of men and women in arrays
-         if (count < size * size) {
-            men.get(count / size).rank[count%size]= inFile.nextInt();
-         } else {
-            women.get((count - size * size) / size).rank[count%size]=inFile.nextInt();
-         }
-         count++;
-      }
-      // test the input.txt
+        public void readfile (String fileName) throws Exception {
+            Scanner inFile = new Scanner(new File(fileName));
+            int count = 0;
+            int size = inFile.nextInt();
+            men = new ArrayList<>();
+            women = new ArrayList<>();
+            for (int i = 0; i < size * 2; i++) {
+                // initial the men and women in arrays
+                if (i < size)
+                    men.add(new Man(size));
+                else
+                    women.add(new Woman(size));
+            }
+            while (inFile.hasNext()) {
+                // initial the rank of men and women in arrays
+                if (count < size * size) {
+                    men.get(count / size).rank[inFile.nextInt()] = count % size;
+                } else {
+                    women.get((count - size * size) / size).rank[inFile.nextInt()] = count % size;
+                }
+                count++;
+            }
+            // test the input.txt
 //       for (int e:men.get(1).rank) {
 //          System.out.println(e);
 //       }
-   
-   }
 
-   public int stableM() {
-      int matched = 0;
-      HashMap<Woman, Man> chioce = new HashMap<>();
-      while (matched < men.size()) {
-         int s = matched;
-         for (s = 0; s < men.size(); s++) {
-            for (int e : men.get(s).rank) {
-               if (women.get(e).free) {
-                  women.get(e).free = false;
-                  chioce.put(women.get(e), men.get(s));
-                  matched++;
-               } else {
-                  if ( > 1)
-                  break;
-               }
+        }
+
+        public HashMap stableM() {
+            int matched = 0;
+            HashMap<Woman, Man> chioce = new HashMap<>();
+            while (matched < men.size()) {
+                int manIndex = matched;
+                for (manIndex = 0; manIndex < men.size(); manIndex++) {
+                    for (int e : men.get(manIndex).rank) {
+                        if (women.get(e).free) {
+                            women.get(e).free = false;
+                            chioce.put(women.get(e), men.get(manIndex));
+                            matched++;
+                        } else {
+                            if (women.get(e).rank[manIndex] < women.get(e).rank[men.indexOf(chioce.get(women.get(e)))]) {
+                                //men.indexOf(chioce.get(women.get(e))) >)
+                                chioce.remove(women.get(e));
+                                chioce.put(women.get(e), men.get(manIndex));
+                                matched++;
+                            }
+                        }
+                    }
+                }
             }
-         }
-      }
-      return 0;
-   }
-}
+            return chioce;
+        }
+    }
+
